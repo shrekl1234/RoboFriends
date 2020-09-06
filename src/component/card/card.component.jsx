ab6772ded41b068d35e5cc3fa1da4a5ce8359202
shrekl1234 <shrekl1234@users.noreply.github.com>
2020-09-06 15:32:51 +0530

    size 200x200

--- src/component/card/card.component.jsx
+++ src/component/card/card.component.jsx
@@ -7,7 +7,7 @@ const Card = ({id, name, email}) => {
 
     return (
       <div className="card bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-        <img src={`https://robohash.org/${id}.png?set=set2&120x120`} alt="Robots Photos" />
+        <img src={`https://robohash.org/${id}.png?set=set2&size=200x200`} alt="Robots Photos" />
         <div>
           <h2>{name}</h2>
           <p>{email}</p>
